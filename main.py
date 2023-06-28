1edf3ea9fcb554a9d647f5691e296b2fec71716f
lexxai <lexxai@users.noreply.github.com>
2023-06-28 18:51:05 +0300

    FUNCTIONS_ARGS for decorator

--- main.py
+++ main.py
@@ -4,80 +4,91 @@
 def parse_input(command_line: str) -> tuple[str, list]:
     for command in COMMANDS:
         if command_line.lower().startswith(command):
-            args = command_line.lstrip(command).strip().split(" ",2)
+            args = command_line.lstrip(command).strip().split(" ",1)
+            args = (s.strip() for s in args)
             return command, args
-    return command_line.lower(),[]
-    # pars = command_line.split()
-    # if len(pars) > 1:
-    #     return pars[0].lower(), pars[1:]
-    # else:
-    #     return command_line.lower(),[]
+    return command_line.lower(),()
+
 
 def input_error(func):
     def wrapper(*args):
-        #print(" Before")
+
+        parameters = FUNCTIONS_ARGS.get(func.__name__,FUNCTIONS_ARGS["default"])   
+
+        #print(" Before ",func.__name__,args,len(args),parameters)
+
+        if not args[0] or (len(args) < parameters):
+            return "Sorry, not enough parameters. Use help for more information."
         try:
             res = func(*args)
-        except (KeyError, ValueError, IndexError)   as e:
-            print("**** Exception fix",e)
+        except (KeyError, ValueError, IndexError):
+            return "Sorry, the settings may be incorrect. Please use the help for more information."
         except Exception as e:
-            print("**** Exception other",e)
+            return "**** Exception other" + e
         else:
             #print(" After")
             return res
-    return wrapper
+    return wrapper    
+
 
 @input_error
 def handler_add(user=None, phone=None, *args) -> str:
     #print("handler_add")
+    user_data[user] = phone
+    return "Done"
 
-    if user is None:
-        return "the user name is not entered"
+    # if user is None:
+    #     return "the user name is not entered"
 
-    if phone is None:
-        return "user phone number is not entered"
+    # if phone is None:
+    #     return "user phone number is not entered"
     
-    if not phone.isdecimal():
-        return "the user's phone number is entered incorrectly"
+    # if not phone.isdecimal():
+    #     return "the user's phone number is entered incorrectly"
 
-    if user not in user_data:
-        user_data[user] = phone
-        return f"Phone of user ({user}) was added"
-    else:
-        return f"User ({user}) already present, maybe want to change ?"
+    # if user not in user_data:
+    #     user_data[user] = phone
+    #     return f"Phone of user ({user}) was added"
+    # else:
+    #     return f"User ({user}) already present, maybe want to change ?"
 
 @input_error
 def handler_change(user=None, phone=None, *args) -> str:
     #print("handler_change")
+    user_data[user]
+    user_data[user] = phone
+    return "Done"
 
-    if user is None:
-        return "the user name is not entered"
+    # if user is None:
+    #     return "the user name is not entered"
 
-    if phone is None:
-        return "user phone number is not entered"
+    # if phone is None:
+    #     return "user phone number is not entered"
     
-    if not phone.isdecimal():
-        return "the user's phone number is entered incorrectly"
+    # if not phone.isdecimal():
+    #     return "the user's phone number is entered incorrectly"
 
-    if user in user_data:
-        user_data[user] = phone
-        return f"Phone of user ({user}) was changed"
-    else:
-        return f"User({user}) not found, maybe want to add it at first ?"
+    # if user in user_data:
+    #     user_data[user] = phone
+    #     return f"Phone of user ({user}) was changed"
+    # else:
+    #     return f"User({user}) not found, maybe want to add it at first ?"
 
 @input_error
 def handler_phone(user=None, *args) -> str:
 
-    if user is None:
-        return "the user name is not entered"
+    return user_data[user]
 
-    if user in user_data:
-        return user_data.get(user)
-    else:
-        return f"User({user}) not found, maybe want to add it at first ?"
+    # if user is None:
+    #     return "the user name is not entered"
+
+    # if user in user_data:
+    #     return user_data.get(user)
+    # else:
+    #     return f"User({user}) not found, maybe want to add it at first ?"
 
 
-@input_error
+
 def handler_show_all(*args) -> str:
     if len(user_data.keys()):
         result = []
@@ -112,16 +123,20 @@ COMMANDS = {
     "show all": handler_show_all,
     "help": handler_help
 }
+FUNCTIONS_ARGS = {
+    "default": 2,
+    "handler_phone": 1
+}
 COMMANDS_HELP = {
     "hello": "Just hello",
-    "add": "add user and phone",
-    "change": "change user's phone",
-    "phone": "show user's phone" ,
-    "show all": "show all user's phone",
-    "help": "List of commands  and their description",
-    "exit": "exit of bot",
-    "close": "exit of bot" ,
-    "good bye": "exit of bot"
+    "add": "Add user and phone. Required username and phone.",
+    "change": "Change user's phone. Required username and phone.",
+    "phone": "Show user's phone. Required username." ,
+    "show all": "Show all user phone numbers.",
+    "help": "List of commands  and their description.",
+    "exit": "Exit of bot.",
+    "close": "Exit of bot." ,
+    "good bye": "Exit of bot."
 }
 
 COMMANDSF = {
